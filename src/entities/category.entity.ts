21a455e3b30e0de8b1c7fbbb0593d24ed133e4d9
CamposLeo95 <leocampos.995@gmail.com>
2024-02-14 19:09:47 -0300

    criando primeira entity

---
+++ src/entities/category.entity.ts
@@ -0,0 +1,16 @@
+interface Categoryprops {
+  id?: string;
+  name: string;
+  color: string;
+}
+export class Category {
+  public id?: string;
+  public name: string;
+  public color: string;
+
+  constructor(props: Categoryprops) {
+    this.id = props.id;
+    this.name = props.name;
+    this.color = props.color;
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,5 @@ import { baseRoutes } from './base.route';
 
 export const routes = Router();
 
-//! Essa Rota usamos apenas como a principal mostrando as descrições do package.json
+//* Essa Rota usamos apenas como a principal mostrando as descrições do package.json
 routes.use('/', baseRoutes);
